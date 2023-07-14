6f7533ce2768823c0a0ac0ee0710f52f33bb45e8
Seb <83839857+sebseb0802@users.noreply.github.com>
2023-07-14 19:06:01 +0100

    Website starting to look presentable
    - About Me needs to be shortened
    - More photos need to be added
    - Resume needs to be added

---
+++ script.js
@@ -0,0 +1,77 @@
+/* Strava button */
+const StravaButton = document.createElement('StravaButton');
+
+StravaButton.style.width = '50px';
+StravaButton.style.height = '50px';
+
+StravaButton.style.border = 'none';
+
+StravaButton.style.position = 'fixed';
+/*StravaButton.style.bottom = '370px';
+StravaButton.style.right = '20px'; */
+StravaButton.style.bottom = '20px';
+StravaButton.style.right = '700px';
+
+StravaButton.addEventListener('click', () => {
+    window.location.href = "https://www.strava.com/athletes/12444464"
+});
+
+StravaButton.style.backgroundImage = "url('https://cdn.icon-icons.com/icons2/2699/PNG/512/strava_logo_icon_170697.png')";
+/*Decrease image size*/
+StravaButton.style.backgroundSize = "cover";
+/*Center the image*/
+StravaButton.style.backgroundPosition = "center";
+
+document.body.appendChild(StravaButton);
+
+/* Linkedin button */
+const LinkedinButton = document.createElement('LinkedinButton');
+
+LinkedinButton.style.width = '50px';
+LinkedinButton.style.height = '50px';
+
+LinkedinButton.style.border = 'none';
+
+LinkedinButton.style.position = 'fixed';
+/*LinkedinButton.style.bottom = '300px';
+LinkedinButton.style.right = '20px'; */
+LinkedinButton.style.bottom = '20px';
+LinkedinButton.style.right = '610px';
+
+LinkedinButton.addEventListener('click', () => {
+    window.location.href = "https://www.linkedin.com/in/kennethwestling";
+});
+
+LinkedinButton.style.backgroundImage = "url('https://cdn.worldvectorlogo.com/logos/linkedin-icon-2.svg')";
+/*Decrease image size*/
+LinkedinButton.style.backgroundSize = "cover";
+/*Center the image*/
+LinkedinButton.style.backgroundPosition = "center";
+
+document.body.appendChild(LinkedinButton);
+
+/* Instagram button */
+const InstagramButton = document.createElement('InstagramButton');
+
+InstagramButton.style.width = '50px';
+InstagramButton.style.height = '50px';
+
+InstagramButton.style.border = 'none';
+
+InstagramButton.style.position = 'fixed';
+/*InstagramButton.style.bottom = '230px';
+InstagramButton.style.right = '20px'; */
+InstagramButton.style.bottom = '20px';
+InstagramButton.style.right = '520px';
+
+InstagramButton.addEventListener('click', () => {
+    window.location.href = "https://www.instagram.com/in/kennethwestling";
+});
+
+InstagramButton.style.backgroundImage = "url('https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-instagram-icon-png-image_6315974.png')";
+/*Decrease image size*/
+InstagramButton.style.backgroundSize = "cover";
+/*Center the image*/
+InstagramButton.style.backgroundPosition = "center";
+
+document.body.appendChild(InstagramButton);
